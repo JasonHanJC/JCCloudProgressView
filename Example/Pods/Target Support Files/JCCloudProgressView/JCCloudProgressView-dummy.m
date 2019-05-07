2404b0bd0eda8c72dc4ff88e056f8cbed668a11e
JasonHan1990 <namrie1990@gmail.com>
2019-05-07 13:44:08 -0700

    Init

---
+++ Example/Pods/Target Support Files/JCCloudProgressView/JCCloudProgressView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JCCloudProgressView : NSObject
+@end
+@implementation PodsDummy_JCCloudProgressView
+@end
---
+++ Example/Pods/Target Support Files/Pods-JCCloudProgressView_Example/Pods-JCCloudProgressView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JCCloudProgressView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JCCloudProgressView_Example
+@end
